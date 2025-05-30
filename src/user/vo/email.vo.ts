010378cc574e954f054c1da23b82e6963aff363d
ShimJiHoon <wlgns2223@gmail.com>
2025-05-30 20:29:00 +0900

    feat: UserEmailVO 이메일 정규표현식 검사 추가

--- src/user/vo/email.vo.ts
+++ src/user/vo/email.vo.ts
@@ -4,20 +4,33 @@ export class UserEmailVO {
   static readonly constraints = {
     minLen: 1,
     maxLen: 255,
+    emailPattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/,
   };
   #email: string;
 
   constructor(email: string) {
-    if (
-      email.length < UserEmailVO.constraints.minLen ||
-      email.length > UserEmailVO.constraints.maxLen
-    ) {
+    this.#validateLength(email);
+    this.#validateEmailPattern(email);
+
+    this.#email = email;
+  }
+
+  #validateLength(email: string) {
+    const { maxLen, minLen } = UserEmailVO.constraints;
+    if (email.length < minLen || email.length > maxLen) {
       throw new BadRequestException(
         `이메일의 길이는 ${UserEmailVO.constraints.minLen} ~ ${UserEmailVO.constraints.maxLen}자 이어야 합니다.}`,
       );
     }
+  }
 
-    this.#email = email;
+  #validateEmailPattern(email: string) {
+    const { emailPattern } = UserEmailVO.constraints;
+    if (emailPattern.test(email)) {
+      throw new BadRequestException(
+        `${email}은 올바른 이메일 형식이 아닙니다.`,
+      );
+    }
   }
 
   get email(): string {
