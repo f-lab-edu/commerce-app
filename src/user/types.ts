ebd951d00c4e797a36c841ca7e33257cb5e583cc
ShimJiHoon <wlgns2223@gmail.com>
2025-06-03 23:47:47 +0900

    fix: 'buyer' 중복 문자열 제거 및 타입 개선

--- src/user/types.ts
+++ src/user/types.ts
@@ -1,1 +1,8 @@
-export type TRole = 'buyer' | 'seller';
+export const USER_ROLE = {
+  buyer: 'buyer',
+  seller: 'seller',
+} as const;
+
+type UserRoleType = typeof USER_ROLE;
+
+export type TRole = UserRoleType[keyof UserRoleType];
