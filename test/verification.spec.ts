35b0cf588be0e85f1e3f97de65f80ab4539e5c54
ShimJiHoon <wlgns2223@gmail.com>
2025-07-01 01:35:30 +0900

    test: sendCode test 케이스 수정

--- test/verification.spec.ts
+++ test/verification.spec.ts
@@ -1,15 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VerificationApplicationServiceImpl } from '../src/verification/verification.applicationServiceImpl';
 import { VerificationService } from '../src/verification/verification.service';
-import { VeriSendStrategy } from '../src/verification/strategy/veriSendStrategy.interface';
-import { VeriStrategyFactory } from '../src/verification/strategy/strategy.factory';
 import { VeriCodeVO } from '../src/verification/vo/code.vo';
 import { SendCodeCommand } from '../src/verification/command/sendCode.command';
-import {
-  EmailSendException,
-  VerificationCodeSendException,
-  VerificationValidExists,
-} from '../src/common/exception/service.exception';
+
 import {
   EmailVerificationEntity,
   PersistedEmailVerificationEntity,
@@ -22,35 +16,15 @@ import { VerifyCodeCommand } from '../src/verification/command/verifyCode.comman
 describe('인증번호 발송 서비스 테스트 VerificationApplicationService', () => {
   let sut: VerificationApplicationServiceImpl;
 
-  const fakeEmail = 'test@gmail.com';
-  const sendCodeCommandMock = new SendCodeCommand(fakeEmail);
-  let verificationServiceMock: jest.Mocked<
-    Pick<
-      VerificationService,
-      | 'findLatestPendingVeri'
-      | 'hasStillValidVeri'
-      | 'saveVeriSendInfo'
-      | 'updateVeriInfo'
-    >
-  >;
-
-  let veriSendStrategyMock: Partial<VeriSendStrategy>;
-  let veriStrategyFactoryMock: Partial<VeriStrategyFactory>;
+  let verificationServiceMock: jest.Mocked<VerificationService>;
 
   beforeEach(async () => {
-    verificationServiceMock = {
-      hasStillValidVeri: jest.fn(),
+    const mockVerificationService = {
+      isSendBlocked: jest.fn(),
       saveVeriSendInfo: jest.fn(),
       updateVeriInfo: jest.fn(),
       findLatestPendingVeri: jest.fn(),
-    };
-
-    veriSendStrategyMock = {
-      send: jest.fn(),
-    };
-
-    veriStrategyFactoryMock = {
-      getStrategy: jest.fn().mockReturnValue(veriSendStrategyMock),
+      sendAndSave: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -58,18 +32,13 @@ describe('인증번호 발송 서비스 테스트 VerificationApplicationService
         VerificationApplicationServiceImpl,
         {
           provide: VerificationService,
-          useValue: verificationServiceMock,
-        },
-        {
-          provide: VeriStrategyFactory,
-          useValue: veriStrategyFactoryMock,
+          useValue: mockVerificationService,
         },
       ],
     }).compile();
 
     sut = module.get(VerificationApplicationServiceImpl);
     verificationServiceMock = module.get(VerificationService);
-    veriStrategyFactoryMock = module.get(VeriStrategyFactory);
   });
 
   afterEach(() => {
@@ -79,82 +48,31 @@ describe('인증번호 발송 서비스 테스트 VerificationApplicationService
   it('모킹 서비스들이 정의되어야합니다.', () => {
     expect(sut).toBeDefined();
     expect(verificationServiceMock).toBeDefined();
-    expect(veriStrategyFactoryMock).toBeDefined();
   });
 
-  it(`발송후 ${VeriCodeVO.constraints.expireInMinute}이 지나지 않은 이메일이 있으면 에러를 던집니다.`, async () => {
-    (verificationServiceMock.hasStillValidVeri as jest.Mock).mockResolvedValue(
-      true,
-    );
-    await expect(sut.sendCode(sendCodeCommandMock)).rejects.toThrow(
-      VerificationValidExists,
-    );
-  });
+  describe('sendCode 테스트 케이스', () => {
+    const fakeEmail = 'test@gmail.com';
+    const sendCodeCommandMock = new SendCodeCommand(fakeEmail);
 
-  it('인증정보 발송 상태를 데이터베이스 저장에 실패하면 에러를 던집니다.', async () => {
-    (verificationServiceMock.hasStillValidVeri as jest.Mock).mockResolvedValue(
-      false,
-    );
+    it('인증정보를 전달하면 전달된 인증채널로 인증코드를 전달한다.', async () => {
+      const mockCode = '123456';
+      jest.spyOn(VeriCodeVO, 'generate').mockReturnValue(mockCode);
+      verificationServiceMock.isSendBlocked.mockResolvedValue(false);
 
-    (verificationServiceMock.saveVeriSendInfo as jest.Mock).mockRejectedValue(
-      new Error('DB save failed'),
-    );
-    await expect(sut.sendCode(sendCodeCommandMock)).rejects.toThrow(
-      VerificationCodeSendException,
-    );
-  });
+      const verificationEntityMock: Partial<PersistedEmailVerificationEntity> =
+        {
+          email: new UserEmailVO(fakeEmail),
+          code: new VeriCodeVO(mockCode),
+          status: VERIFICATION_STATUS.PENDING,
+        };
+      verificationServiceMock.sendAndSave.mockResolvedValue(
+        verificationEntityMock as any,
+      );
 
-  it('이메일 발송에 실패하면 에러를 던지고 인증정보 상태를 업데이트합니다.', async () => {
-    (verificationServiceMock.hasStillValidVeri as jest.Mock).mockResolvedValue(
-      false,
-    );
-    const fakeId = 1;
-    const mockEntity: Partial<PersistedEmailVerificationEntity> = {
-      id: fakeId,
-    };
-    (verificationServiceMock.saveVeriSendInfo as jest.Mock).mockResolvedValue(
-      mockEntity,
-    );
-    const mockErrorMessage = '이메일 전송 실패';
-    (veriSendStrategyMock.send as jest.Mock).mockRejectedValue(
-      new EmailSendException(mockErrorMessage),
-    );
-    const updateVeriInfoSpy = jest.spyOn(
-      verificationServiceMock as jest.Mocked<VerificationService>,
-      'updateVeriInfo',
-    );
+      const result = await sut.sendCode(sendCodeCommandMock);
 
-    await expect(sut.sendCode(sendCodeCommandMock)).rejects.toThrow(
-      VerificationCodeSendException,
-    );
-    expect(updateVeriInfoSpy).toHaveBeenCalledWith(
-      new UpdateVeriCommand({
-        id: mockEntity.id!,
-        status: {
-          status: VERIFICATION_STATUS.FAIL,
-          errorMessage: mockErrorMessage,
-        },
-      }),
-    );
-  });
-
-  it('인증정보를 성공적으로 발송하고 데이터베이스에 저장하면 해당 정보를 반환합니다.', async () => {
-    (verificationServiceMock.hasStillValidVeri as jest.Mock).mockResolvedValue(
-      false,
-    );
-    const fakeId = 1;
-    const mockEntity: Partial<PersistedEmailVerificationEntity> = {
-      id: fakeId,
-      email: new UserEmailVO(fakeEmail),
-      status: VERIFICATION_STATUS.PENDING,
-    };
-    (verificationServiceMock.saveVeriSendInfo as jest.Mock).mockResolvedValue(
-      mockEntity,
-    );
-    (veriSendStrategyMock.send as jest.Mock).mockResolvedValue(undefined);
-
-    const result = await sut.sendCode(sendCodeCommandMock);
-    expect(result).toEqual(mockEntity);
+      expect(result).toEqual(expect.objectContaining(verificationEntityMock));
+    });
   });
 
   describe('verifyCode 테스트 케이스', () => {
