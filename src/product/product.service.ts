79efb847eaf83479df5dd9469e6b7a81ef8d4ae4
ShimJiHoon <wlgns2223@gmail.com>
2025-07-18 16:12:37 +0900

    refactor: query builder 형식으로 변경하였습니다.

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,38 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { PersistedProductEntity, ProductEntity } from './entity/product.entity';
-import { Repository } from 'typeorm';
-
-type Range = {
-  from: string;
-  to: string;
-};
+import { ProductDataAccess, Range } from './product.repository';
 
 @Injectable()
 export class ProductService {
-  constructor(
-    @InjectRepository(ProductEntity)
-    private productRepository: Repository<PersistedProductEntity>,
-  ) {}
+  constructor(private productDataAccess: ProductDataAccess) {}
 
-  async getPopularTopK(limit: string, month: string, price: Range) {
-    return await this.productRepository.query(
-      `SELECT p.id, p.name, sum(od.quantity) AS total_quantity
-        FROM products as p
-        JOIN
-            order_details AS od ON p.id = od.productId
-        JOIN
-            orders AS o ON o.id = od.productId
-        WHERE
-            p.price >= ? AND p.price <= ?
-            AND o.createdAt >= DATE_SUB(NOW(), INTERVAL ? MONTH )
-        GROUP BY
-            p.id, p.name
-        ORDER BY
-            total_quantity DESC
-        LIMIT ?
-        `,
-      [price.from, price.to, month, limit],
-    );
+  async getPopularTopK(limit: number, month: number, price: Range) {
+    return await this.productDataAccess.getPopularTopK(limit, month, price);
   }
 }
