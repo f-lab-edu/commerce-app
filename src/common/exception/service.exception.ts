8fe8ffe1167cc0dbf27baf78d84e8e8881bfdcb5
ShimJiHoon <wlgns2223@gmail.com>
2025-07-25 10:33:07 +0900

    refactor: 예외 클래스 파라미터 타입 변경

--- src/common/exception/service.exception.ts
+++ src/common/exception/service.exception.ts
@@ -1,19 +1,21 @@
 import { HttpStatus } from '@nestjs/common';
 import { HttpStatusCode } from '../decorator/httpCode.decorator';
 
+type ExceptionParams = {
+  clientMsg: string;
+  devMsg?: string;
+  option?: ErrorOptions;
+};
+
 export abstract class CustomException extends Error {
-  protected debuggingMessage: string | undefined;
-  constructor(
-    message: string, // 클라이언트로 보낼 메세지입니다.
-    debuggingMessage?: string, // 디버깅용 메세지입니다.
-    option?: ErrorOptions,
-  ) {
-    super(message, option);
-    this.debuggingMessage = debuggingMessage ?? message;
+  protected devMsg: string | undefined;
+  constructor(params: ExceptionParams) {
+    super(params.clientMsg, params.option);
+    this.devMsg = params.devMsg ?? params.clientMsg;
   }
 
   getDebuggingMessage() {
-    return this.debuggingMessage;
+    return this.devMsg;
   }
 }
 
@@ -21,39 +23,39 @@ export class VerificationException extends CustomException {}
 
 @HttpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR)
 export class VerificationCodeSendException extends VerificationException {
-  constructor(message: string) {
-    super(message);
+  constructor(param: ExceptionParams) {
+    super(param);
   }
 }
 
 @HttpStatusCode(HttpStatus.CONFLICT)
 export class VerificationCodeAlreadySentException extends VerificationException {
-  constructor(message: string) {
-    super(message);
+  constructor(param: ExceptionParams) {
+    super(param);
   }
 }
 @HttpStatusCode(HttpStatus.NOT_FOUND)
 export class VerificationCodeNotFoundException extends VerificationException {
-  constructor(message: string) {
-    super(message);
+  constructor(param: ExceptionParams) {
+    super(param);
   }
 }
 @HttpStatusCode(HttpStatus.BAD_REQUEST)
 export class VerificationExpiredException extends VerificationException {
-  constructor(message: string) {
-    super(message);
+  constructor(param: ExceptionParams) {
+    super(param);
   }
 }
 @HttpStatusCode(HttpStatus.BAD_REQUEST)
 export class VerificationCodeMismatchException extends VerificationException {
-  constructor(message: string) {
-    super(message);
+  constructor(param: ExceptionParams) {
+    super(param);
   }
 }
 @HttpStatusCode(HttpStatus.BAD_REQUEST)
 export class VerificationCodeAlreadyVerifiedException extends VerificationException {
-  constructor(message: string) {
-    super(message);
+  constructor(param: ExceptionParams) {
+    super(param);
   }
 }
 
