163fdc4cbe0465793650f0d0bdbad1cabe8bb5aa
ShimJiHoon <wlgns2223@gmail.com>
2025-08-08 16:26:14 +0900

    chore: 데코레이터 주석 추가

--- src/common/decorator/transaction.decorator.ts
+++ src/common/decorator/transaction.decorator.ts
@@ -24,6 +24,10 @@ export const Transactional = Reflector.createDecorator<boolean>({
   transform: () => true,
 });
 
+/**
+ * APP 모듈에 사용되는 트랜잭션용 전역 인터셉터
+ * 메타 데이터를 확인해 트랜잭션요 엔터티 매니저를 생성
+ */
 @Injectable()
 export class TransactionInterceptor implements NestInterceptor {
   constructor(
